6bcf333a773647f2773201db4b3100bcc5126cff
Jarod Atienzo <Jarodmendozaatienzo@gmail.com>
2025-12-10 01:44:53 -0800

    Manager object created with Add and Remove player methods

---
+++ final/TeamManagerApp/Models/Manager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace TeamManagerApp.Models
+{
+    public class Manager
+    {
+
+        public string ManagerName {get; private set;}
+        public string TeamName {get; private set;}
+        public int WaiverPosition {get; set;}
+        public HashSet<BasketballPlayer> PlayersList;
+
+
+
+        public Manager(string managerName, string teamName, int waiverPosition)
+        {
+            ManagerName = managerName;
+            TeamName = teamName;
+            WaiverPosition = waiverPosition;
+            PlayersList =  new HashSet<BasketballPlayer>();
+        }
+
+        public bool AddPlayer(BasketballPlayer player)
+        {
+            return PlayersList.Add(player);
+        }
+
+        public bool RemovePlayer(int playerId)
+        {
+            BasketballPlayer playerToRemove = FindPlayerByID(playerId);
+            
+            if(playerToRemove == null)
+            {
+                return false;
+            }
+            
+            return PlayersList.Remove(playerToRemove);
+
+        }
+
+        // Helper Methods
+        public BasketballPlayer FindPlayerByID(int playerId)
+        {
+            foreach (BasketballPlayer player in PlayersList)
+            {
+                if(player.Id == playerId)
+                {
+                    return player;
+                }
+            }
+            return null;
+        }
+    }
+}
